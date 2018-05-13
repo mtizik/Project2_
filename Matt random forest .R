c0ae8a74a63db1d4ddfcee96a1c26e596835a545
mtizik <35985760+mtizik@users.noreply.github.com>
2018-05-13 11:04:20 -0700

    Add files via upload

---
+++ Matt random forest .R
@@ -0,0 +1,69 @@
+library(dplyr)
+install.packages('caret')
+install.packages("rpart")
+library(caret)
+library(ggplot2)
+library(rpart)
+library(randomForest)
+library(heuristica)
+
+setwd("Prediction Project")
+Data<- "training.csv"
+Table <- read.csv(Data)
+View(Table)
+Test<- "testing.csv"
+Testt <- read.csv(Test)
+View(Testt)
+
+#delete columns that only contain NA
+Testc <- Testt[, colSums(is.na(Testt)) != nrow(Testt)]
+Testc2 <- Testc[, -2:-7]
+
+#create a value that contains the answers to the training set 
+Answer <- Table[, 160]
+
+
+
+
+#find which columns are now in both Table and Testc
+Colc <- intersect(colnames(Table), colnames(Testc2))
+
+
+
+#get rid of columns that contain 
+Tablec <- Table[, Colc]
+Testc3 <- Testc2[, Colc]
+
+View(Tablec)
+
+View(Testc3)
+#add answers back into table 
+Tablec2 <- cbind(Tablec, Answer)
+
+# remove unwanted data frames 
+
+rm(Testt)
+rm(Table)
+rm(Testc2)
+rm(Testc)
+
+#split Table data into a train and testing index 
+
+Training1 <- sample(1:nrow(Tablec2), size= .8* nrow(Tablec2))
+
+TrainingData <- Tablec2[Training1, ]
+TestData <- Tablec2[-Training1, ]
+
+TestData1 <- TestData[, -TestData$Answer]
+
+
+TrainD1 <- randomForest(Answer ~ . , data= TrainingData, importance= T, ntree= 100 )
+
+Predict1 <- predict(TrainD1, TestData)
+
+Solution <- predict(TrainD1, Testc3)
+
+CMatrix <- table(Predict1, TestData$Answer )
+
+
+
